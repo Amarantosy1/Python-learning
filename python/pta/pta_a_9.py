844fc9f16d40a74e9efd1e5321331ee4ad85efc1
Amarantos <3240102412@zju.edu.cn>
2024-10-13 20:11:15 +0800

    pta_a_9 promoted

--- python/pta/pta_a_9.py
+++ python/pta/pta_a_9.py
@@ -11,3 +11,21 @@ elif count == 1:
         print("B")
     else:
         print("A")
+
+#优化
+a = input().split()
+j = a[2]
+
+# 使用列表切片获取需要比较的元素
+elements_to_compare = a[:2]
+
+# 使用 count 方法计算 j 在 elements_to_compare 中出现的次数
+count = elements_to_compare.count(j)
+
+if count == 0:
+    print("C")
+elif count == 1:
+    if a[0] == j:
+        print("B")
+    else:
+        print("A")
