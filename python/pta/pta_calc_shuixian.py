21173b6aeb0a16d27cddb11f8ec6fecf7b56f974
Amarantos <3240102412@zju.edu.cn>
2024-10-17 19:25:14 +0800

    shuixian-promoted

--- python/pta/pta_calc_shuixian.py
+++ python/pta/pta_calc_shuixian.py
@@ -16,3 +16,21 @@ for i in range(pow(10,n-1),pow(10,n)):
         
 for x in num_lst:
     print(x)
+
+#优化
+# 计算水仙花数
+def sum_pow_list(lst, n):
+    return sum(int(i) ** n for i in lst)
+
+n = int(input())
+num_lst = []
+lower_bound = 10 ** (n - 1)
+upper_bound = 10 ** n
+
+for i in range(lower_bound, upper_bound):
+    num_split = str(i)
+    if sum_pow_list(num_split, n) == i:
+        num_lst.append(i)
+
+for x in num_lst:
+    print(x)
