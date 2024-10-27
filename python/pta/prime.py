72afc6a9583588d432795526d6bbc5c8669f3670
Amarantos <3240102412@zju.edu.cn>
2024-10-27 21:34:16 +0800

    prime added

---
+++ python/pta/prime.py
@@ -0,0 +1,13 @@
+def sieve_of_eratosthenes(n):
+    primes = [True] * (n + 1)
+    p = 2
+    while p * p <= n:
+        if primes[p]:
+            for i in range(p * p, n + 1, p):
+                primes[i] = False
+        p += 1
+    return [p for p in range(2, n + 1) if primes[p]]
+
+# 示例用法
+N = 30
+print(sieve_of_eratosthenes(N))
